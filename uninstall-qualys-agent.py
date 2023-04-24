117849b0a09ca69ae3d15866bfaaa0078859b83b
worldwidez <33632102+worldwidez@users.noreply.github.com>
2023-04-24 13:55:39 -0400

    Delete uninstall-qualys-agent.py

--- uninstall-qualys-agent.py
+++
@@ -1,7 +0,0 @@
-# Get all Qualys agents installed on the system
-$qualys_agents = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -like "*Qualys*"}
-
-# Uninstall all Qualys agents
-foreach ($agent in $qualys_agents) {
-    $agent.Uninstall()
-}
