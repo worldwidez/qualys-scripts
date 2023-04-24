7dbb4cacf104aa4897817d8c663c521d4129c5fd
worldwidez <33632102+worldwidez@users.noreply.github.com>
2023-04-24 13:57:38 -0400

    Create chef-uininstall-qualys-agent.ps1

---
+++ chef-uininstall-qualys-agent.ps1
@@ -0,0 +1,11 @@
+powershell_script 'Uninstall Qualys Agents' do
+  code <<-EOH
+    # Get all Qualys agents installed on the system
+    $qualys_agents = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -like "*Qualys*"}
+
+    # Uninstall all Qualys agents
+    foreach ($agent in $qualys_agents) {
+        $agent.Uninstall()
+    }
+  EOH
+end
